e5238a63298935841e9fb8bc43df9f158e8234bb
Guite <info@guite.de>
2013-10-24 16:01:02 +0200

    CI work

--- production/de.guite.modulestudio.locales.build/begin.sh
+++ production/de.guite.modulestudio.locales.build/begin.sh
@@ -2,3 +2,18 @@
 WORKSPACE=$1
 echo "Linking project git repository"
 ln -sfT /var/lib/jenkins/jobs/MOST-0_Fetch-1_Product/workspace/scm "$WORKSPACE/refProduct"
+
+echo "Linking meta model git repository"
+ln -sfT /var/lib/jenkins/jobs/MOST-0_Fetch-1_Product/workspace/scm "$WORKSPACE/refMetaModel"
+
+echo "Linking textual git repository"
+ln -sfT /var/lib/jenkins/jobs/MOST-0_Fetch-1_Product/workspace/scm "$WORKSPACE/refTextual"
+
+echo "Linking graphical git repository"
+ln -sfT /var/lib/jenkins/jobs/MOST-0_Fetch-1_Product/workspace/scm "$WORKSPACE/refEditors"
+
+echo "Linking generator git repository"
+ln -sfT /var/lib/jenkins/jobs/MOST-0_Fetch-1_Product/workspace/scm "$WORKSPACE/refGenerator"
+
+echo "Linking help git repository"
+ln -sfT /var/lib/jenkins/jobs/MOST-0_Fetch-1_Product/workspace/scm "$WORKSPACE/refHelp"
